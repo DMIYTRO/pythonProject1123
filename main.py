0ce84d6c85547c581a8c6eb5d22552719ed13812
ASUS <1kuperster@gmail.com>
2022-06-22 10:55:56 +0300

    test

---
+++ main.py
@@ -0,0 +1,28 @@
+import smtplib
+import os
+
+
+
+def send_email(messege):
+    sender = "1kuperster@gmail.com"
+    password = os.getenv("DT2Ma5s7m4iF")
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        server.sendmail(sender, sender, messege)
+
+        return "yes"
+    except Exception as _ex:
+        return f"{_ex}\nCheck your login inn psaword please!"
+
+
+def main():
+    messege = input("test")
+    print(send_email(messege=messege))
+
+
+if __name__ == "__main__":
+    main()
