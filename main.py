3b2030f18b6ba946c529a33e5c321523f116cb96
ASUS <1kuperster@gmail.com>
2022-06-25 23:19:03 +0300

    ner

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import subprocess
+import asyncio
+
+
+async def async_func():
+    print("запуск...")
+    await asyncio.sleep(3)
+    print("...done")
+
+
+async def main():
+    print("step")
+    await async_func()
+
+
+asyncio.run(main())
+
